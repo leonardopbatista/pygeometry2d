0fe444668d9fd61626d0dc55626ebd7d2a322ad9
Leonardo Pires Batista <leonardopbatista98@gmail.com>
2025-03-22 08:42:23 -0300

    Atualização das URLs

--- setup.py
+++ setup.py
@@ -4,11 +4,16 @@ with open("README.md", "r", encoding='utf-8') as f:
     readme = f.read()
 
 setup(name='pygeometry2d',
-    version='2.0.3',
+    version='1.0.0',
     license='MIT License',
     author='Leonardo Pires Batista',
     long_description=readme,
     long_description_content_type="text/markdown",
+    url = 'https://github.com/leonardopbatista/pygeometry2d',
+    project_urls = {
+        'Código fonte': 'https://github.com/leonardopbatista/pygeometry2d',
+        'Download': 'https://github.com/leonardopbatista/pygeometry2d'
+    },
     author_email='leonardopbatista98@gmail.com',
     keywords='geometry 2d',
     description=u'Biblioteca de geometria 2D',
